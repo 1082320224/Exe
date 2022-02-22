077eda0e5925fe07782ba2e6c5cf5dce996aa39f
1082320224 <100131517+1082320224@users.noreply.github.com>
2022-02-22 19:43:20 +0800

    Create Ex5.cpp

---
+++ Ex5.cpp
@@ -0,0 +1,12 @@
+# Exe#include<stdio.h>
+#include <iostream>
+using namespace std;
+int main() {
+  int myAge = 14;
+  int myheight = 160;
+  int myweigth = 58;
+  printf(" I am %d \n", myAge); 
+  printf(" I am %d \n", myheight);
+  printf(" I am %d \n", myweigth);
+  return 0; 
+}
