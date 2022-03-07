2beae5d9cf989135dee76e73073207157c5d3ade
1082320224 <100131517+1082320224@users.noreply.github.com>
2022-03-07 19:50:01 +0800

    Create Ex6.cpp

---
+++ Ex6.cpp
@@ -0,0 +1,11 @@
+# Exe#include<stdio.h>
+int main (void) {
+     printf(" hello Tienyun "); 
+     printf(" my height is %d m %d cm \n",1,80);
+     printf(" this building is %6d cm \n", 890);
+     printf(" this building is %6d cm \n", 7890);
+     printf(" this building is %06d cm \n", 67890);
+     printf(" this building is %06d cm \n", 567890);
+     printf(" this building is %06d cm \n", 1234567890);
+     printf(" pi = %0.10f \n ", 3.1415926535897932384626433832795);
+}
