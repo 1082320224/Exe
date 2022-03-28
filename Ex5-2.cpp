68f4b36195b69a04be6295ee011212df0144b716
1082320224 <100131517+1082320224@users.noreply.github.com>
2022-03-28 19:32:03 +0800

    Create Ex5-2.cpp

---
+++ Ex5-2.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf(" please input a \n");
+    scanf(" %d",&a);
+    if(a == 1)
+        printf(" monday \n");
+    else if (a == 2)
+        printf(" tuesday \n");
+    else if (a == 3)
+        printf(" wednesday \n");
+    else if (a == 2)
+        printf(" thursday \n");
+    else if (a == 2)
+        printf(" friday \n");
+    else if (a == 2)
+        printf(" saturday \n");
+    else if (a == 2)
+        printf(" sunday \n");
+        
+    return 0;
+}
